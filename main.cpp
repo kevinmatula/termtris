0df0990b53b6407e89d11a466d8f8471f9777da0
kevinmatulaa <matulakevin91@gmail.com>
2026-02-04 23:12:38 -0500

    set buffer to entire string instead of returning per character

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 const int SCENE_WIDTH = 80;
 const int SCENE_HEIGHT = 30;
 
+const int GAME_WIDTH = 60;
+const int GAME_HEIGHT = SCENE_HEIGHT;
+
 // cool-ass cleanup function that stops CPU mid instruction to run this command.
 void cleanup(int) {
   cout << "\x1b[?1049l\x1b[?25h" << flush;
@@ -18,10 +21,24 @@ void cleanup(int) {
 
 // places markers at top and bottom of screen to visually display bounds
 void setBounds(vector<vector<char>> &buf) {
-  for (int x = 0; x < SCENE_WIDTH; x++) {
+  for (int x = 0; x < GAME_WIDTH; x++) {
     buf[0][x] = '-';
     buf[SCENE_HEIGHT - 1][x] = '-';
   }
+}
+
+// This function takes the entire buffer and turns it into a string
+// This functions reduces flicker by rendering entire string at once and then
+// outputting that!
+string renderScreen(vector<vector<char>> buf) {
+  string screen = "";
+  for (int y = 0; y < SCENE_HEIGHT; y++) {
+    for (int x = 0; x < SCENE_WIDTH; x++) {
+      screen += buf[y][x];
+    }
+    screen += "\n";
+  }
+  return screen;
 }
 
 int main() {
@@ -38,18 +55,11 @@ int main() {
   // create visual upper and lower bounds
   while (1) {
 
-    cout << "\x1b[2J\x1b[H";
-    for (int y = 0; y < SCENE_HEIGHT; y++) {
-      for (int x = 0; x < SCENE_WIDTH; x++) {
-        cout << buffer[y][x];
-      }
-      cout << "\n";
-    }
+    cout << "\x1b[H";
+
+    cout << renderScreen(buffer);
+
     cout << flush;
     usleep(9000);
   }
 }
-
-// TODO: consider different sized terminals, like "termtris --small" and
-// "termtris --large"
-// TODO: Consider writing an entire string instead of line by line via for loop
